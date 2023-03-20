83ad56a86a6d62eb64987b1131eb0e9544cf0fd0
kenshu <kenshu.app@gmail.com>
2023-03-20 14:11:33 +0900

    fix:06-7~8 arrayFunc/arraySeparate

---
+++ 06/arrayFnuc.php
@@ -0,0 +1,7 @@
+<?php
+$domain = 'http://example.com?';
+$text = 'PHP　MySQL　Laravel';
+
+echo $domain . http_build_query(explode('　', $text));
+
+/* http://example.com?0=PHP&1=MySQL&2=Laravel */
---
+++ 06/arraySeparate.php
@@ -0,0 +1,21 @@
+<?php
+$alph = 'A-B-C';
+
+// [A,B,C]
+$arr = explode('-', $alph);
+
+// [A,B,C] + D
+array_push($arr, 'D');
+
+// 4 <= [A,B,C,D]
+$num = count($arr);
+
+// [A,B,C,D] + 4個
+array_push($arr, $num . '個');
+
+/* A | B | C | D | 4個 */
+echo implode(' | ', $arr);
+
+echo '<pre>';
+print_r($arr);
+echo '</pre>';
