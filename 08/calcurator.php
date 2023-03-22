788671d3ed25d11a28ba822cc214e5122528cbae
kenshu <kenshu.app@gmail.com>
2023-03-22 10:22:17 +0900

    fix:08-3 cart

--- 08/calcurator.php
+++ 08/calcurator.php
@@ -18,8 +18,8 @@ $result = $num1 + $num2;
     <h1>計算</h1>
     <h2><?=$num1?> + <?=$num2?> = <?=$result?></h2>
     <form action="" method="post">
-        <input type="text" name="num1" size="2" maxlength="2" value="<?=$num1?>">+
-        <input type="text" name="num2" size="2" maxlength="2" value="<?=$num2?>">=
+        <input type="text" name="num1" value="<?=$num1?>">+
+        <input type="text" name="num2" value="<?=$num2?>">=
         <p><input type="submit" value="計算"></p>
     </form>
 </body>
