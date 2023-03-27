df8ae92f2464568a16bc02fe8f119b5305450067
kenshu <kenshu.app@gmail.com>
2023-03-27 14:45:09 +0900

    fix:12-3 contstruct

--- 12/member1.php
+++ 12/member1.php
@@ -6,8 +6,31 @@ class Member
     public $age;
     public $address;
 
+    /**
+     * メンバーの名前を受け、すぐに表示
+     *
+     * @param string $name
+     */
+    public function __construct(string $name = '名無し')
+    {
+        $this->name = $name;
+        echo '<p>' . $this->name . 'が生成されました</p>';
+    }
+
+    public function __destruct()
+    {
+        echo '<p>' . $this->name . 'が破棄されます</p>';
+    }
+
+    public function __call($name, $args)
+    {
+        echo $name . '関数、または' . implode('や', $args) .
+            'となる引数は存在しません。';
+    }
+
     public function showInfo()
     {
+        echo __CLASS__ . '<br>';
         echo '<ul>';
         echo '<li>名前：' . $this->name . '</li>';
         echo '<li>年齢：' . $this->age . '</li>';
@@ -16,13 +39,12 @@ class Member
     }
 }
 
-$m1 = new Member;
-$m1->name    = '山田太郎';
+$m1 = new Member('山田太郎');
 $m1->age     = 21;
 $m1->address = '東京都';
+$m1->info('test', 10, 40, 50, 60);
 
 $m2 = new Member;
-$m2->name    = '鈴木次郎';
 $m2->age     = 34;
 $m2->address = '大阪府';
 
