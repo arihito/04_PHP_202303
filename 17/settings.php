6ec4436459a2b13958f4986f71abb9913bd44539
ArihitoMatsuda <arihito.m@gmail.com>
2023-03-29 17:05:56 +0900

    update: settings appPassword

--- 17/settings.php
+++ 17/settings.php
@@ -3,7 +3,7 @@ const SMTP_HOST     = 'smtp.gmail.com';
 const SMTP_PORT     = 587;
 const SMTP_PROTOCOL = 'tls';
 const GMAIL_SITE    = 'arihito.m@gmail.com';
-const GMAIL_APPPASS = 'gjsfzfoerbvkbuzi';
+const GMAIL_APPPASS = '';
 // 代替テキスト(送信元のGmailでOK)
 const MAIL_FROM     = ['arihito.m@gmail.com' => '公式サイト'];
 // 複数の送信先の設定
